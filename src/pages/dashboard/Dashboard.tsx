e6523df32522a29bac04a2c3e0911ba8d58f578e
devhasanmia <dev.hasanmia@gmail.com>
2024-10-15 06:10:34 +0600

    something

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,7 +5,6 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen flex flex-col justify-between">
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 p-6">
-      {/* Total Customers */}
       <div className="bg-green-500 p-6 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out">
         <div className="flex items-center justify-between">
           <h1 className="font-medium text-lg mb-2">Total Product</h1>
@@ -13,8 +12,6 @@ const Dashboard = () => {
         </div>
         <p className="text-3xl font-semibold">500</p>
       </div>
-
-      {/* Total Due */}
       <div className="bg-red-500 p-6 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out">
         <div className="flex items-center justify-between">
           <h1 className="font-medium text-lg mb-2">Total Orders</h1>
@@ -22,8 +19,6 @@ const Dashboard = () => {
         </div>
         <p className="text-3xl font-semibold">50</p>
       </div>
-
-      {/* Revenue Collected */}
       <div className="bg-indigo-500 p-6 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out">
         <div className="flex items-center justify-between">
           <h1 className="font-medium text-lg mb-2">Total Earn</h1>
@@ -32,14 +27,6 @@ const Dashboard = () => {
         <p className="text-3xl font-semibold">500 $</p>
       </div>
     </div>
-    {/* <div className="container m-auto">
-      <ReactApexChart
-        options={series.options}
-        series={series.series}
-        type="area"
-        height={350}
-      />
-    </div> */}
   </div>
   )
 }
